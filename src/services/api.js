fd3c37a5259ba2a5f1766d8699f8c71ce60ac13c
easontseng <yhsints.dev.gmail.com>
2024-08-12 04:19:12 +0800

    [Feat]增添當日更新與新增匹配通知資訊

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ const addjobNotifications = async (notificationRepository, notif) => {
     const foundUser = await User.findUserById(user_id);
     // 可能會引發異常的代碼
     if (foundUser) {
-      const message = `總共有 ${notif.count} 筆工作資料符合結果`;
+      const message = `有 ${notif.update} 筆更新，共有 ${notif.count} 筆工作資料符合結果`;
       const link = { url: 'http://localhost:4000/api/filterJobsBySub', data: notif };
       await addNotifications(notificationRepository, user_id, message, link);
       console.log('success');
